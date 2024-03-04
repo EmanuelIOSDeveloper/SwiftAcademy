5ac6147a2e686d653abc6b7cbfead484024cdd8c
Emanuel Pacheco <emanuel.pacheco@itsector.pt>
2024-03-04 19:06:32 +0000

    Add files for protocols

---
+++ Playground/Part 10 - Protocols.playground/Pages/01 - Introduction.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+
+//: # Part 5: Protocols & Inheritance
+//: ## Episode 01: Introduction
+//:
+//:
+//:
+//: [⇒ Next: 02 - Inheritance](@next)
+
+
---
+++ Playground/Part 10 - Protocols.playground/Pages/02 - Inheritance.xcplaygroundpage/Contents.swift
@@ -0,0 +1,47 @@
+//: [⇐ Previous: 01 - Introduction](@previous)
+//: ## Episode 02: Inheritance
+
+struct Grade {
+  var letter: Character
+  var points: Double
+  var credits: Double
+}
+
+class Person {
+  var firstName: String
+  var lastName: String
+  
+  init(firstName: String, lastName: String) {
+    self.firstName = firstName
+    self.lastName = lastName
+  }
+}
+
+class Student: Person {
+  var grades: [Grade] = []
+  
+}
+
+class SchoolBandMember: Student {
+    var minimumPracticeTime = 2
+}
+
+class StudentAthlete: Student {
+    var isEligible: Bool {
+        return grades.filter { $0.letter == "F" } .count < 3
+    }
+}
+
+let jon = Person(firstName: "Jon", lastName: "Snon")
+let jane = Student(firstName: "Jane", lastName: "Snane")
+jon.firstName
+jane.firstName
+
+let historyGrade = Grade(letter: "B", points: 9, credits: 3)
+jane.grades.append(historyGrade)
+
+
+
+
+
+//: [⇒ Next: 03 - Challenge - Inheritance](@next)
